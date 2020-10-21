36462551b8c3b1ec01bd2e703380607ca4f4dc28
Guus De Graeve <guus@massivemedia.eu>
2020-10-21 17:06:15 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,16 @@ if [ -z "$INPUT_SSH_PRIVATE_KEY" ]; then
     exit 1
 fi
 
+if [ -z "$INPUT_STACK_COMPOSE_FILE" ]; then
+    echo "Input INPUT_STACK_COMPOSE_FILE is required!"
+    exit 1
+fi
+
+if [ -z "$INPUT_STACK_NAME" ]; then
+    echo "Input INPUT_STACK_NAME is required!"
+    exit 1
+fi
+
 echo "Registering SSH keys..."
 
 # Save private key to a file and register it with the agent.
@@ -29,4 +39,4 @@ ssh-add "$HOME/.ssh/docker"
 ssh-keyscan -t rsa $INPUT_SSH_HOST >> ~/.ssh/known_hosts
 
 echo "Connecting to $INPUT_SSH_HOST..."
-docker --log-level debug --host ssh://$INPUT_SSH_USER@$INPUT_SSH_HOST "$@" 2>&1
+docker --host ssh://$INPUT_SSH_USER@$INPUT_SSH_HOST stack deploy --compose-file $INPUT_STACK_COMPOSE_FILE --with-registry-auth $INPUT_STACK_NAME 2>&1
